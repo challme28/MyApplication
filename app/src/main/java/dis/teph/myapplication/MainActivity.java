bf12b435a4523a449c49a821e06516ceba741127
challme28 <challme28@gmail.com>
2015-08-31 15:35:49 -0500

    day 2

--- app/src/main/java/dis/teph/myapplication/MainActivity.java
+++ app/src/main/java/dis/teph/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package dis.teph.myapplication;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar= (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        /*
+        NavigationDrawerFragment drawerFragment=(NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);*/
     }
 
     @Override
